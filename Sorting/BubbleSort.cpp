095d2e0c286cb684a50470dc60f5299481050037
AkashRauniyar877 <akashrauniyar877@gmail.com>
2026-02-03 21:21:42 +0530

    ..

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+
+    int arr[6]={5,4,6,1,3,2};
+    int n=6;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n-1-i;j++){
+//             if(arr[j]>arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//             }
+//         }
+//    }
+
+    //optimized bubble sort
+    for(int i=0;i<n;i++){       //n-1 passes
+        bool flag = true;
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                flag = false;
+            }
+        }
+        if(flag== true){ // swap didn't happen
+            break;
+        }
+   }
+   cout<<"\n";
+
+    
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    
+
+}
