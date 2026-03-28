5f72f8dea8aa70f0c02259215ce2a3a0e6690aeb
AkashRauniyar877 <akashrauniyar877@gmail.com>
2026-03-28 11:45:01 +0530

    ..

---
+++ Leetcode/largestRectangleInHistogram84.cpp
@@ -0,0 +1,46 @@
+ #include<iostream>
+#include<stack>
+#include<vector>
+#include<string>
+#include <algorithm>
+using namespace std;
+
+ int largestRectangleArea(vector<int>& arr) {
+        int n = arr.size();
+        int nsi[n];
+        stack<int>st;
+        nsi[n-1] = n;
+        st.push(n-1);
+        for(int i=n-2;i>=0;i--){
+            while(st.size()>0 && arr[st.top()]>= arr[i]) st.pop();
+            if(st.size()==0) nsi[i]= n;
+            else nsi[i] = st.top();
+            st.push(i);
+        }
+
+        //prevoius greater element
+        int psi[n];
+        psi[0] = -1;
+        stack<int>gt;
+        gt.push(0);
+        for(int i =1;i<n;i++){
+            while(gt.size()>0 && arr[gt.top()]>= arr[i]) gt.pop();
+            if(gt.size()==0) psi[i]= -1;
+            else psi[i] = gt.top();
+            gt.push(i);
+        }
+        int maxArea = 0;
+        for(int i=0;i<n;i++){
+            int height = arr[i];
+            int breadth = nsi[i] - psi[i] - 1;
+            int area = height* breadth;
+            maxArea = max(maxArea,area);
+        }
+        return maxArea;
+    }
+
+int main() {
+    vector<int> arr = {2, 1, 5, 6, 2, 3};  
+    int result = largestRectangleArea(arr);
+    cout << "Largest Rectangle Area: " << result << endl;
+}
