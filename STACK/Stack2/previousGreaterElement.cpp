be9daff671e70a75e5993bfb4300475118bc2c39
AkashRauniyar877 <akashrauniyar877@gmail.com>
2026-04-03 21:47:28 +0530

    ..

---
+++ STACK/Stack2/previousGreaterElement.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stack>
+#include<string>
+#include <algorithm>
+using namespace std;
+int main(){
+    int arr[] = {3,1,2,7,4,6,2,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    // pop ans push
+    stack<int> st;
+    int pge[n];
+    pge[0] = -1;
+    st.push(arr[0]); 
+    for(int i=1;i<n;i++){
+        //pop all the elemnts smaller than arr[i]
+        while(st.size()>0 && st.top()<=arr[i]){
+            st.pop();
+        }
+        // mark the ans in nge array
+        if(st.size()==0) pge[i] = -1;
+        else pge[i] = st.top();
+        //pushh the arr[i]
+        st.push(arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        cout<<pge[i]<<" ";
+    }
+  
+
+}
+   
