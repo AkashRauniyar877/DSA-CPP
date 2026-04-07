a076f694048a4a75a36e8ef13d21f2ab294c33d6
AkashRauniyar877 <akashrauniyar877@gmail.com>
2026-04-07 13:13:30 +0530

    ..

--- STACK/Stacks1/using recurssion/recursionInStack.cpp
+++ STACK/Stacks1/using recurssion/recursionInStack.cpp
@@ -19,7 +19,25 @@ void display(stack<int> &st){
     cout<<x<<" ";
     st.push(x);
 
+}
 
+void pushAtBottom(stack<int>& st, int val){
+    if(st.size()==0) {
+        st.push(val);
+        return;
+    }
+    int x = st.top();
+    st.pop();
+    pushAtBottom(st,val);
+    
+    st.push(x);
+}
+void reverse(stack<int>&st){
+    if(st.size()==1) return;
+    int x =  st.top();
+    st.pop();
+    reverse(st);
+    pushAtBottom(st,x);
 }
 
 int main(){
@@ -29,8 +47,13 @@ int main(){
     st.push(30);
     st.push(40);
     st.push(50);
-    displayRev(st);
-    cout<<endl;
+    //displayRev(st);
+   // cout<<endl;
     display(st);
+    //pushAtBottom(st,90);
+    cout<<endl;
+    reverse(st);
+    display(st);
+    
 
 }
