c9fdea571a1306297ec53e52b6cfc86968981450
AkashRauniyar877 <akashrauniyar877@gmail.com>
2026-05-26 23:40:19 +0530

    ...

---
+++ MAPS & SETS/LEC1/maxStringPairs.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<unordered_set>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int maximumNumberOfStringPairs(vector<string>& arr){
+    int n = arr.size();
+    unordered_set<string>s;
+   int count = 0;
+   for(int i=0;i<n;i++){
+    s.insert(arr[i]);
+   }
+   for(int i=0;i<n;i++) {
+    string rev = arr[i];
+    reverse(rev.begin(),rev.end());
+    if(arr[i]==rev) continue;
+    if(s.find(rev)!= s.end()){
+        count++;
+        s.erase(arr[i]); 
+    }
+   }
+   return count;
+}
+int main(){
+    vector<string>arr = {"cd","ac","dc","ca","zz"};
+    cout<<maximumNumberOfStringPairs(arr)<<endl;
+}
